911d249b01d39256fa1d4781ab8e4190253c5e93
Kiran Cherukuri <kirancherukuri@Kirans-MacBook-Air.local>
2021-07-02 15:46:08 -0400

    started bonding curve contract

--- contracts/BondingCurve.sol
+++ contracts/BondingCurve.sol
@@ -5,13 +5,32 @@ pragma solidity ^0.8.4;
  * @title Bonding Curve
  * @author neuroswish
  *
- * Implement bonding curves to govern the pricing of continuous tokens
+ * Implement bonding curves governing the pricing of continuous tokens
  *
  * "All of you Mario, it's all a game"
  */
 
 import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
+import {SafeMath} from "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract BondingCurve is ChainlinkClient {
     using Chainlink for ChainlinkRequest;
+    using SafeMath for uint256;
+
+    address private oracle;
+    bytes32 private jobId;
+    uint256 private fee;
+
+    uint256 reserveRatio = 1.div(3);
+    uint256 m = 1.div(1000000);
+
+    constructor() public {
+        setPublicChainlinkToken();
+    }
+
+    function getInitialPrice(
+        uint256 storage reserveRatio,
+        uint256 storage m,
+        uint256 _tokens
+    ) public returns (uint256) {}
 }
