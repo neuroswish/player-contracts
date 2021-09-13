d6bd81c1775c1cf9ae5a061a6150cd7d01b29436
Kiran Cherukuri <kirancherukuri@Kirans-MacBook-Air.local>
2021-09-13 01:04:48 -0400

    inherited erc721 specs

--- contracts/Cryptomedia.sol
+++ contracts/Cryptomedia.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.4;
 import "./BondingCurve.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "./PlayerERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "./interfaces/IBondingCurve.sol";
 import "./libraries/Base64.sol";
@@ -15,18 +17,18 @@ import "./libraries/Base64.sol";
  * "All of you Mario, it's all a game"
  */
 
-contract Cryptomedia is ReentrancyGuardUpgradeable {
+contract Cryptomedia is ERC721, ReentrancyGuard, Initializable {
     // ======== Interface addresses ========
     address public bondingCurve; // bonding curve interface address
     string[] public choices; // user choices
 
     // ======== Continuous token params ========
-    string public name; // cryptomedia name
+    //string private name; // cryptomedia name
     uint32 public reserveRatio; // reserve ratio in ppm
     uint32 public ppm; // token units
     uint256 public poolBalance; // ETH balance in contract pool
-    uint256 public totalSupply; // total supply of tokens in circulation
-    mapping(address => uint256) public balanceOf; // mapping of an address to that user's total token balance for this contract
+    uint256 public continuousTotalSupply; // total supply of tokens in circulation
+    mapping(address => uint256) public continuousBalanceOf; // mapping of an address to that user's total token balance for this contract
 
     // ======== Player params ========
     mapping(address => bool) public created; // mapping of an address to bool representing whether address has created a layer
@@ -42,18 +44,22 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
     event Buy(
         address indexed buyer,
         uint256 poolBalance,
-        uint256 totalSupply,
+        uint256 continuousTotalSupply,
         uint256 tokens,
         uint256 price
     );
     event Sell(
         address indexed seller,
         uint256 poolBalance,
-        uint256 totalSupply,
+        uint256 continuousTotalSupply,
         uint256 tokens,
         uint256 eth
     );
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    event continuousTransfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
     event LayerCreated(address indexed creator, string text);
     event LayerUpdated(address indexed creator, string newText);
     event LayerRemoved(address indexed creator);
@@ -63,7 +69,7 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
      * @notice Check to see if address holds tokens
      */
     modifier holder(address user) {
-        require(balanceOf[user] > 0, "MUST HOLD TOKENS");
+        require(continuousBalanceOf[user] > 0, "MUST HOLD TOKENS");
         _;
     }
 
@@ -82,15 +88,16 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
      */
     function initialize(
         string calldata _name,
+        string calldata _symbol,
         address _bondingCurve,
         string[] calldata _choices
-    ) public payable initializer {
+    ) public initializer {
         reserveRatio = 333333;
         ppm = 1000000;
-        name = _name;
+        //name = _name;
         bondingCurve = _bondingCurve;
         choices = _choices;
-        __ReentrancyGuard_init();
+        ERC721.initialize(_name, _symbol);
     }
 
     // ======== Functions ========
@@ -108,22 +115,28 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
         require(_minTokensReturned > 0, "INVALID SLIPPAGE");
         // calculate tokens returned
         uint256 tokensReturned;
-        if (totalSupply == 0) {
+        if (continuousTotalSupply == 0) {
             tokensReturned = IBondingCurve(bondingCurve)
                 .calculateInitializationReturn(_price, reserveRatio);
         } else {
             tokensReturned = IBondingCurve(bondingCurve)
                 .calculatePurchaseReturn(
-                    totalSupply,
+                    continuousTotalSupply,
                     poolBalance,
                     reserveRatio,
                     _price
                 );
         }
         // mint tokens for buyer
-        _mint(_creator, tokensReturned);
+        _continuousMint(_creator, tokensReturned);
         poolBalance += _price;
-        emit Buy(msg.sender, poolBalance, totalSupply, tokensReturned, _price);
+        emit Buy(
+            msg.sender,
+            poolBalance,
+            continuousTotalSupply,
+            tokensReturned,
+            _price
+        );
     }
 
     /**
@@ -135,18 +148,24 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
         require(_minETHReturned > 0, "INVALID SLIPPAGE");
         // calculate ETH returned
         uint256 ethReturned = IBondingCurve(bondingCurve).calculateSaleReturn(
-            totalSupply,
+            continuousTotalSupply,
             poolBalance,
             reserveRatio,
-            balanceOf[_creator]
+            continuousBalanceOf[_creator]
         );
         require(ethReturned >= _minETHReturned, "SLIPPAGE");
         // burn tokens
-        uint256 sellAmt = balanceOf[_creator];
-        _burn(_creator, sellAmt);
+        uint256 sellAmt = continuousBalanceOf[_creator];
+        _continuousBurn(_creator, sellAmt);
         poolBalance -= ethReturned;
         sendValue(_creator, ethReturned);
-        emit Sell(_creator, poolBalance, totalSupply, sellAmt, ethReturned);
+        emit Sell(
+            _creator,
+            poolBalance,
+            continuousTotalSupply,
+            sellAmt,
+            ethReturned
+        );
     }
 
     /**
@@ -172,20 +191,6 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
         emit LayerCreated(msg.sender, text);
     }
 
-    // /**
-    //  * @notice Update a cryptomedia layer
-    //  * @dev Emits a LayerUpdated event upon success; callable by token-holding creators
-    //  */
-    // function updateLayer(string memory _newText)
-    //     public
-    //     holder(msg.sender)
-    //     creator(msg.sender)
-    // {
-    //     require(bytes(_newText).length < 300, "INPUT TOO LARGE");
-    //     addressToLayer[msg.sender].text = _newText;
-    //     emit LayerUpdated(msg.sender, _newText);
-    // }
-
     /**
      * @notice Remove a cryptomedia layer
      * @dev Emits a LayerRemoved event upon success; callable by token-holding creators
@@ -203,31 +208,25 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
         emit LayerRemoved(msg.sender);
     }
 
-    // ============ Public helper functions ============
-
     /**
      * @notice Return the layer information for a given address that has created or curated a layer
      * @dev Must specify user address; returns layer creator address and layer URI; reverts if address has not created or curated a layer
      */
-    function getLayer(address _user)
-        public
-        view
-        returns (address, string memory)
-    {
+    function tokenURI(address _user) public view returns (string memory) {
         require(created[_user], "NO MATCHING LAYERS");
         string memory layerText = addressToLayer[_user].text;
         string[2] memory parts;
         parts[
             0
-        ] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">';
+        ] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: black; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="white" /><text x="10" y="20" class="base">';
         parts[1] = layerText;
         string memory output = string(abi.encodePacked(parts[0], parts[1]));
         string memory json = Base64.encode(
             bytes(
                 string(
                     abi.encodePacked(
-                        '{"name": "VERSE $',
-                        upper(name),
+                        '{"name": ',
+                        upper(ERC721.name()),
                         '", "image": "data:image/svg+xml;base64,',
                         Base64.encode(bytes(output)),
                         '"}'
@@ -238,7 +237,7 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
         output = string(
             abi.encodePacked("data:application/json;base64,", json)
         );
-        return (_user, addressToLayer[_user].text);
+        return (output);
     }
 
     // ============ Utility ============
@@ -252,28 +251,30 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
 
         // solhint-disable-next-line avoid-low-level-calls, avoid-call-value
         (bool success, ) = payable(recipient).call{value: amount}("REVERTED");
-        emit Transfer(address(this), msg.sender, amount);
-        require(success);
+        require(
+            success,
+            "Address: unable to send value, recipient may have reverted"
+        );
     }
 
     /**
      * @notice Mints tokens
      * @dev Emits a Transfer event upon success
      */
-    function _mint(address _to, uint256 _value) private {
-        totalSupply = totalSupply + _value;
-        balanceOf[_to] = balanceOf[_to] + _value;
-        emit Transfer(address(0), _to, _value);
+    function _continuousMint(address _to, uint256 _value) private {
+        continuousTotalSupply = continuousTotalSupply + _value;
+        continuousBalanceOf[_to] = continuousBalanceOf[_to] + _value;
+        emit continuousTransfer(address(0), _to, _value);
     }
 
     /**
      * @notice Burns tokens
      * @dev Emits a Transfer event upon success
      */
-    function _burn(address _from, uint256 _value) private {
-        balanceOf[_from] = balanceOf[_from] - _value;
-        totalSupply = totalSupply - _value;
-        emit Transfer(_from, address(0), _value);
+    function _continuousBurn(address _from, uint256 _value) private {
+        continuousBalanceOf[_from] = continuousBalanceOf[_from] - _value;
+        continuousTotalSupply = continuousTotalSupply - _value;
+        emit continuousTransfer(_from, address(0), _value);
     }
 
     /**
@@ -311,4 +312,6 @@ contract Cryptomedia is ReentrancyGuardUpgradeable {
 
         return _b1;
     }
+
+    // ============ ERC-721 ============
 }
